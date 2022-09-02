4b6afeb1fc2b84d1e473a205c59f62d5ace1da5f
Kuhy <kwh1019_@naver.com>
2022-09-02 15:13:52 +0900

    [김우헌] 2일차 문제 제출 (10814)

---
+++ 10814/10814_python_김우헌.py
@@ -0,0 +1,13 @@
+'''
+baekjoon s5 나이순정렬
+https://www.acmicpc.net/problem/10814
+'''
+
+N = int(input())
+user_list = []
+for i in range(N):
+    age, name = input().split()
+    user_list.append([i, int(age), name])
+user_list.sort(key = lambda x: (x[1], x[0]))
+for u in user_list:
+    print(str(u[1]) + ' ' + u[2] + '\n')
