163a196588fc65b231c5a5ed9cc84c00fcc8af80
Kuhy <kwh1019_@naver.com>
2022-09-13 18:27:20 +0900

    add comment

--- src/22941/22941_python_김우헌.py
+++ src/22941/22941_python_김우헌.py
@@ -1,7 +1,9 @@
 '''
 baekjoon s2 RPG 마스터 오명진
 https://www.acmicpc.net/problem/22941
-1. 마왕을 죽이는데 소요되는 턴 계산 (마왕체력 // 용사공격력)
+1. 마왕이 체력을 회복할 수 있는지 확인 -> 체력회복한다면 마왕체력에 S 더하기
+- 기준점 P보다 공격력이 낮거나, 치다가 0<n<P 사이의 체력 n 이 될 경우 무조건 체력회복
+2. 마왕 잡는데 걸리는 턴수 <= 유저 죽는데 걸리는 턴수 라면 승리
 '''
 
 import sys
@@ -11,7 +13,7 @@ input = sys.stdin.readline
 a_hp, a_power, b_hp, b_power = map(int, input().split())
 P, S = map(int, input().split())
 
-if not a_power >= b_hp and 0 < b_hp - a_power <= P: #(b_hp - P) % a_power + P >= a_power:
+if a_power <= P or 0 < b_hp % a_power <= P:
     b_hp += S
 
 print('Victory!') if math.ceil(b_hp / a_power) <= math.ceil(a_hp / b_power) else print('gg')
