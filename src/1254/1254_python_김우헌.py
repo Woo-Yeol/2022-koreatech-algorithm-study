f97fdc17d8b7e092cfbe118278c0231c833aeb8c
Kuhy <kwh1019_@naver.com>
2022-09-16 23:03:53 +0900

    [김우헌] 13일차 문제 풀이

---
+++ src/1254/1254_python_김우헌.py
@@ -0,0 +1,26 @@
+'''
+baekjoon s2 팰린드롬 만들기
+https://www.acmicpc.net/problem/1254
+2초, 문자열 길이 최대 50
+'''
+
+import sys
+
+input = sys.stdin.readline
+S = input().rstrip()
+result = len(S)
+init_length = len(S)
+
+while True:
+    if result % 2 == 0:
+        compare_length = len(S[result//2:])
+        first = S[:result//2][::-1][:compare_length]
+        second = S[result//2:]
+    else:
+        compare_length = len(S[result//2+1:])
+        first = S[:result//2][::-1][:compare_length]
+        second = S[result//2+1:]
+    if first == second:
+        break
+    result += 1
+print(result)
