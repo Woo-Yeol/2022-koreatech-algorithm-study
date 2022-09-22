84526c292a3a8b604377028b56fe495f54f0ceb6
{zsmalla} <{malla7454@gmail.com}>
2022-09-22 09:57:13 +0900

    [임지수] 15일차 문제 풀이 (16198)

--- src/16198/16198_python_임지수.py
+++ src/16198/16198_python_임지수.py
@@ -1,26 +1,28 @@
 import sys
+from itertools import permutations
+from copy import deepcopy
 
 input = sys.stdin.readline
 
 N = int(input())
 lst = list(map(int, input().split()))
 
-def getMaxEnergyInPossibleRange(lst):
-    maxEnergy = 0
-    for i in range(1, N-1):
-        E = lst[i-1]*lst[i+1]
-        if E > maxEnergy:
-            maxEnergy = E
-            selectIdx = i
-        elif E == maxEnergy:
-            selectIdx = i if lst[selectIdx] > lst[i] else selectIdx     # 에너지 같으면 더 작은거
-        
-    return maxEnergy, [lst[idx] for idx in range(N) if idx != selectIdx]
-            
-totalEnergy = 0
-while N != 2:
-    getEnergy, lst = getMaxEnergyInPossibleRange(lst)
-    totalEnergy += getEnergy
-    N -= 1
+cases = permutations(range(1, N-1))     # 인덱스 제외 순서의 모든 경우의 수
+energies = []
 
-print(totalEnergy)
+for case in cases:
+    tmp = deepcopy(lst)                 # 다음 케이스에서 활용을 고려
+    energy = 0
+    removedIndex = []                   # 제외한 인덱스 관리
+    for removeIndex in case:
+        decreaseIndex = len([i for i in removedIndex if i < removeIndex])       # 앞에서 줄어든 만큼 인덱스 감소
+        removedIndex.append(removeIndex)
+        removeIndex -= decreaseIndex
+
+        thisEnergy = tmp[removeIndex-1] * tmp[removeIndex+1]
+        tmp.pop(removeIndex)
+        energy += thisEnergy
+
+    energies.append(energy)
+
+print(max(energies))
